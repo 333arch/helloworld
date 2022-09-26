778c00e932f8c3667125f792a5da6d301a6cc4e2
Archie Rees <archierees@Archies-MBP.lan>
2022-09-26 18:58:02 +0100

    first commit

---
+++ Helloworld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void JobQuote(string Job)
+        {
+            if (Job == "Engineer")
+            {
+                Console.WriteLine("The engineer has been, and is, the maker of history.");
+            }
+            else if (Job == "Developer")
+            {
+                Console.WriteLine("Logical thinking, passion and perseverance is paint on your pallet");
+            }
+            else if (Job == "Analyst")
+            {
+                Console.WriteLine("Seeing what other people can't see gives you great vision");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry we can not find a quote for your job");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your job");
+            string TempTing = Console.ReadLine();
+            JobQuote(TempTing);
+        }
+    }
+}
